77a591b961fc8ed56e3c402021a6589c95821109
jfrancisco.vicente <jfrancisco.vicente@pragsis.com>
2016-10-28 09:11:11 +0200

    First commit

---
+++ SparkSample/src/main/java/com/pragsis/spark/SparkSample/Prueba.java
@@ -0,0 +1,13 @@
+package com.pragsis.spark.SparkSample;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		
+		String s = "Hola como	estás yo muy bien";
+		 String[] titles = s.split("\\t");
+		System.out.println("+++++++++++++++++++++"+titles[0]);
+
+	}
+
+}
